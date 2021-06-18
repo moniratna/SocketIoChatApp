5c499eb97bb74dade70b1b190d115e857445ab31
Moniratna <moniratna.munshi123@gmail.com>
2021-06-18 23:39:28 +0530

    finished

--- server.js
+++ server.js
@@ -1,20 +1,54 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io');
+const io = require('socket.io').listen(server);
 
-const port = 3001;
+let users = [];
 
-app.set('views', path.join(__dirname, "views"));
+const port = 3000;
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Static path
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (re,res, next)=> {
-    res.render('index');
+// Socket.io connect
+io.sockets.on('connection', (socket) => {
+  // Set Username
+  socket.on('set user', (data, callback) => {
+    console.log('setting');
+    if(users.indexOf(data) != -1){
+      callback(false);
+    } else {
+      callback(true);
+      socket.username = data;
+      users.push(socket.username);
+      updateUsers();
+    }
+  });
+
+  socket.on('send message', function(data){
+    io.sockets.emit('show message', {msg: data, user: socket.username});
+  });
+
+  socket.on('disconnect', function(data){
+    if(!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsers();
+  });
+
+  function updateUsers(){
+    io.sockets.emit('users', users);
+  }
 });
 
-server.listen(port, ()=>{
-    console.log('server started on port '+ port);
-})
+app.get('/', (req, res, next)=> {
+  res.render('index');
+});
+
+server.listen(port, () => {
+  console.log('Server started on port '+port);
+});
